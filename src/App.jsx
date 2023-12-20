416812b79db594f8df7b2443a991707d170227b5
Bode20029 <jry.bode@gmail.com>
2023-12-20 19:57:07 +0700

    complete loadPoke function part1

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,35 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [poke, setPoke] = useState("")
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("")
   useEffect( () => {
-    let AbortController = new AbortController();
+    let abortController = new AbortController();
+
+    const loadPoke = async () => {
+      try  {
+
+        setLoading(true);
+        let response = await axios.get(`https://pokeapi.co/api/v2/1`, {
+          signal: abortController.signal
+        });
+
+        setPoke(response.data);
+        setError("");
+      } catch (error) {
+        setError("Something went wrong")
+      } finally {
+        setLoading(false);
+      }
+    }
+    return () => abortController.abort()
   },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
