1728c84eac4d155f37ec2454de5b06492f151930
Bode20029 <jry.bode@gmail.com>
2023-12-20 20:08:55 +0700

    debug 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 
@@ -9,28 +7,34 @@ function App() {
   const [poke, setPoke] = useState("")
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("")
+  
   useEffect( () => {
+    
     let abortController = new AbortController();
 
     const loadPoke = async () => {
       try  {
 
         setLoading(true);
-        let response = await axios.get(`https://pokeapi.co/api/v2/1`, {
+        let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/1`, {
           signal: abortController.signal
         });
 
         setPoke(response.data);
         setError("");
       } catch (error) {
-        setError("Something went wrong")
+        setError("Something went wrong",error)
       } finally {
         setLoading(false);
       }
     }
+
+    loadPoke();
+
+    
     return () => abortController.abort()
   },[])
-
+  console.log(poke)
   return (
     <>
       
