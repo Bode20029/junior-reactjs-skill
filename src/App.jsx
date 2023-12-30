a363ee15a42317d89ce5cf05c48a2af0854ee71f
Bode20029 <jry.bode@gmail.com>
2023-12-30 15:12:06 +0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 import FavPoke from './components/FavPoke';
-
+import ReactLoading from 'react-loading';
 // components
 
 
@@ -55,21 +55,31 @@ function App() {
   console.log("Pokemon ID", number)
 
   return (
-    <div>
-      <h1>{poke?.name} </h1>
-      <button onClick={addFav}>Add to favorite</button>
-      <br/>
-      <img src={poke?.sprites?.other?.home?.front_default} alt={poke?.name} />
-      <ul>
-        {poke?.abilities?.map((abil, idx) => (
-          <li key={idx}>{abil.ability.name}</li>
-        ))}
-      </ul>
-      <button onClick={prevPoke}>Previous</button>
-      <button onClick={nextPoke}>Next</button>
+    <div className="max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
       <div>
-        <FavPoke fav={fav} />
+        {loading ? 
+          <ReactLoading type='spin' color='black' height={'20%'} width={'20%'}  />
+          :
+          <>
+            <h1>{poke?.name} </h1>
+            <button onClick={addFav}>Add to favorite</button>
+            <br/>
+            <img src={poke?.sprites?.other?.home?.front_default} alt={poke?.name} />
+            <ul>
+              {poke?.abilities?.map((abil, idx) => (
+                <li key={idx}>{abil.ability.name}</li>
+              ))}
+            </ul>
+            <button onClick={prevPoke}>Previous</button>
+            <button onClick={nextPoke}>Next</button>
+          </>}
       </div>
+      <div>
+        <h2>Your favorite Pokemon</h2>
+        {fav.length > 0 ? <FavPoke fav={fav} /> : <div className='flex h-full justify-center items-center'> <p> No favorite pokemon...</p> </div>}
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/FavPoke.jsx
+++ src/components/FavPoke.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import LikePoke from './LikePoke'
 
 function FavPoke({fav}) {
   return (
-    <div>
-        <h2>Your favorite Pokemon</h2>
+    <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
+        
         {fav?.map((data, idx) => (
             <div key={idx}>
             <h3>{data.name}</h3>
             <img src={data?.sprites?.other?.home?.front_default} alt="" />
+            <LikePoke/>
             </div>
         ))}
     </div>
